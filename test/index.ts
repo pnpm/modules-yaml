9ca9badec2bc3a01c45f77ec23b951f3f6a0b326
Zoltan Kochan <z@kochan.io>
2018-06-22 22:10:49 +0300

    test: fix

--- test/index.ts
+++ test/index.ts
@@ -3,11 +3,9 @@ import {read, write, Modules} from '@pnpm/modules-yaml'
 import tempy = require('tempy')
 
 test('write() and read()', async (t) => {
-  const modulesYaml: Modules = {
-
-  }
+  const modulesYaml = {} as Modules
   const tempDir = tempy.directory()
   await write(tempDir, modulesYaml)
-  t.deepEqual(await read(tempDir, modulesYaml), modulesYaml)
+  t.deepEqual(await read(tempDir), modulesYaml)
   t.end()
 })
